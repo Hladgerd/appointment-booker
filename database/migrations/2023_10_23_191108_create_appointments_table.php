f8d32d023c5c54eefd42668c9120c2146f9140fa
Hladgerd <hladgerd@gmail.com>
2023-10-24 10:53:41 +0200

    add appointments table migration

---
+++ database/migrations/2023_10_23_191108_create_appointments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * The database connection that should be used by the migration.
+     *
+     * @var string
+     */
+    protected $connection = 'pgsql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name', 2000);
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->foreignId('frequency_id');
+            $table->integer('day')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
