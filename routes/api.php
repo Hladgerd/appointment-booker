f1ed3bc28dc38fdd021a1385d4e18974435c3b71
Hladgerd <hladgerd@gmail.com>
2023-10-25 12:34:56 +0200

    add api route for business hours

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\BusinessTimeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/appointments', [AppointmentController::class, 'index']);
+Route::get('/business-hours', [BusinessTimeController::class, 'index']);
