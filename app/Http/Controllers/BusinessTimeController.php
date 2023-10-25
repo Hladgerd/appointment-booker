9eb3dfb17df7943a046a4df4977839ccea6c2725
Hladgerd <hladgerd@gmail.com>
2023-10-25 13:47:58 +0200

    add business time controller

---
+++ app/Http/Controllers/BusinessTimeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BusinessTime;
+use Illuminate\Http\JsonResponse;
+
+class BusinessTimeController extends Controller
+{
+    /**
+     * Display a listing of the business time.
+     */
+    public function index(): JsonResponse
+    {
+        $businessHours = BusinessTime::all();
+        $events = array();
+
+        foreach ($businessHours as $businessHour) {
+            $events[] = [
+                'rrule' => $businessHour->frequency,
+                'duration' => $businessHour->duration,
+                'display' => 'inverse-background',
+            ];
+        }
+        return response()->json($events);
+    }
+}
